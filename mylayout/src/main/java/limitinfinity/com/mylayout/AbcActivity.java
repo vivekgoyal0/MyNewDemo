c7ffbfa09bb66ee10ad9b724a8ec3979bf4c67c5
Vivek Gupta <vivek@limitinfinity.org>
2018-01-04 12:35:22 +0530

    Updated
    Updated

---
+++ mylayout/src/main/java/limitinfinity/com/mylayout/AbcActivity.java
@@ -0,0 +1,18 @@
+package limitinfinity.com.mylayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Raman on 1/4/2018.
+ */
+
+public class AbcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abc);
+
+    }
+}
